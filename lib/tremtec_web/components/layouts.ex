279d2c8b70fa353a69231cb29da91f39f8cec290
Marco Antônio <ma.souza.junior@gmail.com>
2025-09-25 17:55:06 -0300

    fea: translate footer

--- lib/tremtec_web/components/layouts.ex
+++ lib/tremtec_web/components/layouts.ex
@@ -125,7 +125,7 @@ defmodule TremtecWeb.Layouts do
     ~H"""
     <footer class="footer footer-center p-6 bg-base-200 text-base-content">
       <div>
-        <p>Copyright © {Date.utc_today().year} - Todos os direitos reservados a TremTec</p>
+        <p>Copyright © {Date.utc_today().year} - {gettext("All rights reserved")}</p>
       </div>
     </footer>
     """
