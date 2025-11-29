1fccda94da160deb77a7cd36c5dbb4ead3c00fa2
Marco Antônio <ma.souza.junior@gmail.com>
2025-11-29 09:22:39 -0300

    fea: generate auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :tremtec, :scopes,
+  user: [
+    default: true,
+    module: Tremtec.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Tremtec.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :tremtec,
   ecto_repos: [Tremtec.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
--- lib/tremtec_web/router.ex
+++ lib/tremtec_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TremtecWeb.Router do
   use TremtecWeb, :router
 
+  import TremtecWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,15 +10,12 @@ defmodule TremtecWeb.Router do
     plug :put_root_layout, html: {TremtecWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
 
     plug TremtecWeb.Plug.DetermineLocale,
       supported_locales: Tremtec.Config.supported_locales(),
       default_locale: Tremtec.Config.default_locale(),
       gettext: TremtecWeb.Gettext
-  end
-
-  pipeline :admin_auth do
-    plug TremtecWeb.Plug.AdminBasicAuth, :runtime
   end
 
   pipeline :api do
@@ -31,8 +30,6 @@ defmodule TremtecWeb.Router do
     live "/contact", ContactLive
 
     scope "/admin" do
-      pipe_through [:admin_auth]
-
       live "/messages", Admin.Messages.IndexLive
       live "/messages/:id", Admin.Messages.ShowLive
     end
@@ -58,5 +55,33 @@ defmodule TremtecWeb.Router do
       live_dashboard "/dashboard", metrics: TremtecWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", TremtecWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{TremtecWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", TremtecWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{TremtecWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
