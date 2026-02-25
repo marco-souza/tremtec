5a820403a291f4eb1f1d50819418fefc6e1873bc
Marco <ma.souza.junior@gmail.com>
2026-02-25 14:18:42 -0300

    fea: increment login page to make a email login option

--- src/ui/contents/login.ts
+++ src/ui/contents/login.ts
@@ -1,6 +1,11 @@
 export const loginContent = {
   title: "Sign In - TremTec",
   welcome: "Welcome back, Innovator.",
+  divider: "Or continue with email",
+  email_login: {
+    placeholder: "Enter your email",
+    button: "Continue with Email",
+  },
   tos_privacy: {
     text: "By signing in, you agree to our",
     terms: "Terms",
